df46d4766ad33619a0bf50e9f7c86eba2829eae0
Trey Smith <trey.smith@nasa.gov>
2013-01-23 10:57:25 -0800

    initial app creation from template

---
+++ xgds_planner2/tests.py
@@ -0,0 +1,15 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.test import TestCase
+
+
+class xgds_planner2Test(TestCase):
+    """
+    Tests for xgds_planner2
+    """
+    def test_xgds_planner2(self):
+        pass
