7e0bf782f4b64192bee6c64e321faf73837840b4
Trey Smith <trey.smith@nasa.gov>
2013-03-18 18:32:56 -0700

    added forms.CreatePlanForm

--- xgds_planner2/forms.py
+++ xgds_planner2/forms.py
@@ -4,4 +4,16 @@
 # All Rights Reserved.
 # __END_LICENSE__
 
-# from django import forms
+from django import forms
+
+from xgds_planner2 import xpjson, models
+
+SITE_CHOICES = [(x.id, x.name) for x in models.LIBRARY.sites]
+PLATFORM_CHOICES = [(x.id, x.name) for x in models.LIBRARY.platforms]
+
+
+class CreatePlanForm(forms.Form):
+    planNumber = forms.IntegerField(initial=9999)
+    planVersion = forms.CharField(max_length=1, initial='A')
+    site = forms.ChoiceField(choices=SITE_CHOICES, required=False)
+    platform = forms.ChoiceField(choices=PLATFORM_CHOICES, required=False)
