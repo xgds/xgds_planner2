8b26878c5e90effba967e5101dc6c361871dc5cb
Ted Scharff <e.b.scharff@nasa.gov>
2013-05-01 14:48:34 -0700

    Added forms.js (oops)

---
+++ xgds_planner2/static/xgds_planner2/js/forms.js
@@ -0,0 +1,33 @@
+
+(function(Form){
+    Form.editors.Coordinates = Form.editors.Text.extend({
+        /**
+        * Returns the current editor value
+        * @return {String}
+        */
+        getValue: function() {
+            var str = this.$el.val();
+            var coords = str.split(',');
+            var lng = parseFloat(coords[0]);
+            var lat = parseFloat(coords[1]);
+            return {
+                type: 'Point',
+                coordinates: [lng, lat]
+            };
+        },
+
+        /**
+        * Sets the value of the form element
+        * @param {String}
+        */
+        setValue: function(value) {
+            var lng = value.coordinates[0];
+            var lat = value.coordinates[1];
+            var str = '' + lng + ', ' + lat;
+            this.$el.val(str);
+        },
+
+    });
+
+})(Backbone.Form);
+
