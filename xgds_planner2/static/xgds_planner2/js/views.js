78ac6b2ec74b13f7052fd48ef7c9834ddd2e12be
Tamar Cohen <tamar.e.cohen@nasa.gov>
2013-09-25 10:27:38 -0700

    added cachebuster for tree

--- xgds_planner2/static/xgds_planner2/js/views.js
+++ xgds_planner2/static/xgds_planner2/js/views.js
@@ -712,7 +712,8 @@ app.views.TabNavView = Backbone.Marionette.Layout.extend({
 		gex: ge.gex,
 		mapElement: [],
 		element: [],
-		restoreState: true
+		restoreState: true,
+		bustCache: true
 	    });
 	    app.tree.load();
 	});
