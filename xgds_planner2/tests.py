a5cbbb635590761adba343cf02e53032a5f1935c
Jerome Rasky <jyrome.112@gmail.com>
2013-12-08 20:48:10 -0800

    Use reverse() to find urls in tests

--- xgds_planner2/tests.py
+++ xgds_planner2/tests.py
@@ -7,8 +7,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.test.utils import override_settings
-
-from xgds_planner2 import settings
+from django.core.urlresolvers import reverse
 
 import logging
 
@@ -16,32 +15,33 @@ import logging
 class xgds_planner2Test(TestCase):
     fixtures = ['xgds_planner2_testing.json',
                 'xgds_planner2_testing_auth.json']
-    urls = 'xgds_planner2.testing_urls'
 
     def setUp(self):
         logging.disable(logging.WARNING)
 
     def test_index(self):
-        response = self.client.get('/xgds_planner2/index/', follow=True)
+        response = self.client.get(reverse('planner2_index'), follow=True)
         self.assertEquals(response.status_code, 200)
 
     def test_edit(self):
-        response = self.client.get('/xgds_planner2/edit/1', follow=True)
+        response = self.client.get(reverse('planner2_edit', args=['1']), follow=True)
         self.assertEquals(response.status_code, 200)
 
     def test_doc(self):
-        response = self.client.get('/xgds_planner2/doc/1', follow=True)
+        response = self.client.get(reverse('planner2_doc', args=['1']), follow=True)
         self.assertEquals(response.status_code, 200)
 
     def test_plan_REST(self):
-        response = self.client.get('/xgds_planner2/plan/1/test.json', follow=True)
+        response = self.client.get(reverse('planner2_planREST', args=['1', 'test']), follow=True)
         self.assertEquals(response.status_code, 200)
 
     def test_plan_export(self):
-        response = self.client.get('/xgds_planner2/plan/export/421d0eb5-f04d-4f36-a4f7-503e0ca8ef2e/test.kml', follow=True)
+        response = self.client.get(reverse('planner2_planExport',
+                                           args=['421d0eb5-f04d-4f36-a4f7-503e0ca8ef2e', 'test.kml']),
+                                   follow=True)
         self.assertEquals(response.status_code, 200)
 
     def test_create_plan(self):
         self.client.login(username="vagrant", password="vagrant")
-        response = self.client.get('/xgds_planner2/plan/create/', follow=True)
+        response = self.client.get(reverse('planner2_planCreate'), follow=True)
         self.assertEquals(response.status_code, 200)
