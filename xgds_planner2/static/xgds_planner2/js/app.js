88c8c543c5d35f9e1d8607d633eced5fedd37c77
Tamar Cohen <tamar.e.cohen@nasa.gov>
2014-11-29 10:41:40 -0800

    test readonly

--- xgds_planner2/static/xgds_planner2/js/app.js
+++ xgds_planner2/static/xgds_planner2/js/app.js
@@ -298,8 +298,9 @@ var app = (function($, _, Backbone) {
             });
             app.toolbar.show(new app.views.ToolbarView());
             app.tabs.show(new app.views.TabNavView());
+            
             app.vent.trigger('clearSaveStatus');
-            if (this.options.readOnly){
+            if (this.options.readOnly == true){
                 app.vent.trigger('readOnly');
             }
         });
