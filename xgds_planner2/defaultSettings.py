fc6d935eed0afffe9bfb9b67d26295a198584ed4
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-05-28 18:47:08 -0700

    bathymetry functional

--- xgds_planner2/defaultSettings.py
+++ xgds_planner2/defaultSettings.py
@@ -194,9 +194,9 @@ XGDS_PLANNER_COMMAND_RENDERER_SCRIPTS = ()
 # see xgds_kn for example
 XGDS_PLANNER_COMMAND_RENDERERS = {}
 
-# XGDS_PLANNER_EARTH_LOADED_CALLBACK: The fully qualified name of an
-# extra JavaScript callback to call after Google Earth is loaded.
-XGDS_PLANNER_EARTH_LOADED_CALLBACK = 'null'
+# XGDS_PLANNER_MAP_LOADED_CALLBACK: The fully qualified name of an
+# extra JavaScript callback to call after the map is loaded.
+XGDS_PLANNER_MAP_LOADED_CALLBACK = 'null'
 
 # If this is defined (true) then include the scheduling & flight management features in display
 # IMPORTANT YOU MUST INCLUDE THIS IN SITE SETTINGS
--- xgds_planner2/static/xgds_planner2/js/app.js
+++ xgds_planner2/static/xgds_planner2/js/app.js
@@ -307,6 +307,7 @@ var app = (function($, _, Backbone) {
             app.map = new app.views.OLPlanView({
                 el: '#map'
             });
+            app.vent.trigger('onMapSetup');
             app.toolbar.show(new app.views.ToolbarView());
             app.tabs.show(new app.views.TabNavView());
             
